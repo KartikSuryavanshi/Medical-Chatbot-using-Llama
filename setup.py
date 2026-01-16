f0c6a903ba1c9c0d9a5033a4d39c8ef9b6773bc8
Kartik Suryavanshi <158498247+KartikSuryavanshi@users.noreply.github.com>
2026-01-16 12:43:20 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Kartik Suryavanshi',
+    author_email= 'kartiknileshs@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
